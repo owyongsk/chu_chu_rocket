47e5a4410b77de948ec83e53827cba999a581e80
Edward Fung <hk.fung.edward@gmail.com>
2014-06-14 18:26:16 +0800

    Added arrow logic
    Signed-off-by: Edward Fung <hk.fung.edward@gmail.com>

--- chu_chu_rocket.js
+++ chu_chu_rocket.js
@@ -1,7 +1,28 @@
 Mice = new Meteor.Collection("mice");
 Players = new Meteor.Collection("players");
+SpawnPoints = new Meteor.Collection('spawn_points');
+Arrows = new Meteor.Collection('arrows');
+Destinations = new Meteor.Collection('destinations');
 
-SpawnPoints = [[25,25,'right'],[75,325,'left']] // only two
+var players = { };
+var arrows = {
+  '1' : []
+  , '2' : []
+};
+
+var sps = [
+    {player: 1, top: 25, left:25 , direction:'right'}
+  , {player: 2, top: 75, left:325, direction:'left'}
+  , {player: 1, top: 125, left:25 , direction:'right'}
+  , {player: 2, top: 175, left:325, direction:'left'}
+];
+
+var destinations = [
+    {player: 1, top:  25, left:325 }
+  , {player: 2, top:  75, left: 25 }
+  , {player: 1, top: 125, left:325 }
+  , {player: 2, top: 175, left: 25 }
+];
 
 function move(direction){
   switch(direction) {
@@ -17,28 +38,29 @@ function move(direction){
 }
 
 function directionAfterWall(mouse){
-  if (mouse.top == 25 && mouse.direction == 'up'){
-    return 'right';
-  } else if (mouse.top == 175 && mouse.direction == 'down') {
-    return 'left';
-  } else if (mouse.left == 25 && mouse.direction == 'left') {
-    return 'up';
-  } else if (mouse.left == 325 && mouse.direction == 'right') {
-    return 'down';
+  if (mouse.top <= 25 && mouse.direction == 'up'){
+    return 'right'
+  } else if (mouse.top >= 175 && mouse.direction == 'down') {
+    return 'left'
+  } else if (mouse.left <= 25 && mouse.direction == 'left') {
+    return 'up'
+  } else if (mouse.left >= 325 && mouse.direction == 'right') {
+    return 'down'
   } else {
     return mouse.direction
   }
 }
 
 function scorePoint(mouse){
+  
   if (mouse.top == 75 && mouse.left == 25){
-    Players.update(Players.findOne({number: 1})._id, {$inc: {score: 1}});
+    Players.update(players['1'], {$inc: {score: 1}});
   } else if (mouse.top == 175 && mouse.left == 25) {
-    Players.update(Players.findOne({number: 1})._id, {$inc: {score: 1}});
+    Players.update(players['1'], {$inc: {score: 1}});
   } else if (mouse.top == 25 && mouse.left == 325) {
-    Players.update(Players.findOne({number: 2})._id, {$inc: {score: 1}});
+    Players.update(players['2'], {$inc: {score: 1}});
   } else if (mouse.top == 125 && mouse.left == 325) {
-    Players.update(Players.findOne({number: 2})._id, {$inc: {score: 1}});
+    Players.update(players['2'], {$inc: {score: 1}});
   } else {
     return;
   }
@@ -58,42 +80,115 @@ if (Meteor.isClient) {
     return Players.find({});
   }
 
-  // Template.ccr.events({
-    // 'click input': function () {
-      // Mice.update(Mice.findOne()._id, {$set: {direction: "right"}})
-    // }
-  // });
+  Template.ccr_arrows.arrows = function(){
+    var arrowsTemp = Arrows.find().fetch();
+    _.each(arrowsTemp, function(arrow, i){
+      arrowsTemp[i].top -= 25;
+      arrowsTemp[i].left -= 25;
+    });
+    return arrowsTemp;
+  }
+
+  //Template.ccr.events({
+  //  'click #game .cell': function () {
+  //    console.log('clicked');
+  //  }
+  //});
+}
+
+var canPutArrow = function(pid, pos){
+  // center
+  var x = Math.floor(pos.x / 50) * 50 + 25;
+  var y = Math.floor(pos.y / 50) * 50 + 25;
+
+  var conflictedSp = _.find(sps, function(sp, i){
+    return sp.top == y && sp.left == x;
+  });
+
+  if (conflictedSp){
+    return false;
+  }
+
+  //var arr
+  return true;
+}
+
+Meteor.methods({
+  addArrow: function(playerId, pos, direction){
+    if (canPutArrow(playerId, pos)){
+      if (arrows[playerId].length >= 3){
+        var firstArrowId = arrows[playerId].shift();
+        Arrows.remove({_id: firstArrowId});
+      }
+      var x = Math.floor(pos.x / 50) * 50 + 25;
+      var y = Math.floor(pos.y / 50) * 50 + 25;
+      var aid = Arrows.insert({top: y, left: x, direction: direction});
+      arrows[playerId].push(aid);
+      return aid;
+    }
+    return '';
+  }
+});
+
+var directionAfterArrow = function(mouse){
+  var arrow = Arrows.findOne({top: mouse.top, left: mouse.left});
+  //console.log(arrow);
+  if (arrow){
+    return arrow.direction;
+  }
+
+  return mouse.direction;
 }
 
 if (Meteor.isServer) {
+  var spawningRate = 1000;
+  var lock = false;
   Meteor.setInterval(function() {
-    Mice.find({}).forEach(function (mouse) {
-      var new_direction = directionAfterWall(mouse);
-      Mice.update(mouse._id, {$set: {direction: new_direction }});
-      Mice.update(mouse._id, move(new_direction));
-      // scorePoint(mouse);
+    if (lock == false){
+      lock = true;
+      var toUpdate = [];
+      Mice.find({}).forEach(function (mouse) {
+        mouse.direction = directionAfterArrow(mouse);
+        var new_direction =  directionAfterWall(mouse);
+        data = _.extend({}, {$set: {direction: new_direction }}, move(new_direction));
+        Mice.update(mouse._id, data);
+        scorePoint(mouse);
+      });
+      lock = false;
+    } else {
+      // Do nothing
+    }
+  }, 1000);
+
+  var t = 0;
+  Meteor.setInterval(function(){
+    SpawnPoints.find({}).forEach(function(sp){
+      console.log(sp);
+      delete sp._id;
+      Mice.insert(sp);
     });
-  }, 1000)
+    t++;
+    console.log(t);
+  }, spawningRate);
 
   Meteor.startup(function () {
     // code to run on server at startup
 
     Mice.remove({});    // remove all mice
     Players.remove({}); // remove all players
+    SpawnPoints.remove({});
+    Arrows.remove({});
 
-    _.each(SpawnPoints, function(sp) {
-      Mice.insert({
-        top: sp[0],
-        left: sp[1],
-        direction: sp[2]
-      });
+    _.each(sps, function(sp) {
+      SpawnPoints.insert( sp );
+      Mice.insert(sp);
     });
 
-    Players.insert({
+    players['1'] = Players.insert({
       number: 1,
       score: 0
     });
-    Players.insert({
+    players['2'] = Players.insert({
       number: 2,
       score: 0
     });
--- client/lib/js/app.js
+++ client/lib/js/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 	var gamemap = {};
 	
+  var id = 1;
+
 	gamemap.selector = '#map';
 	gamemap.gen = function(col, row){
 		var $game = $(gamemap.selector);
@@ -15,6 +17,31 @@ $(document).ready(function() {
 			html += '</div>';
 		}
 		$game.html(html);
+
+    var cursorPos = {x:0, y:0};
+    $game.mousemove(function(e){
+      var parentOffset = $(this).offset(); 
+      //or $(this).offset(); if you really just want the current element's offset
+      var relX = e.pageX - parentOffset.left;
+      var relY = e.pageY - parentOffset.top;
+
+      cursorPos.x = relX;
+      cursorPos.y = relY;
+    }); 
+
+    $('body').keydown(function(e){
+      switch (event.which){
+        case 65:
+          console.log('a', cursorPos);
+          var direction = 'up';
+          Meteor.call('addArrow', id, cursorPos, direction, function(err, result){
+            console.log(err);
+            console.log(result);
+          });
+          break;
+      }
+    });
 	};
 	gamemap.gen(7, 4);
+
 });
