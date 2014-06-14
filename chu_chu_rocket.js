08bcb3b006ddc083131877cd806ccc65172179bd
SoonKhen OwYong <dude@owyong.sk>
2014-06-14 14:56:28 +0800

    added wall detection collision

--- chu_chu_rocket.js
+++ chu_chu_rocket.js
@@ -1,7 +1,33 @@
-Grids = new Meteor.Collection("grids");
 Mice = new Meteor.Collection("mice");
 
 SpawnPoints = [[25,25],[25,125]] // only two
+
+function move(direction){
+  switch(direction) {
+    case "up":
+      return {$inc: {top: -50}}
+    case "down":
+      return {$inc: {top: 50}}
+    case "left":
+      return {$inc: {left: -50}}
+    case "right":
+      return {$inc: {left: 50}}
+  }
+}
+
+function directionAfterWall(mouse){
+  if (mouse.top == 25 && mouse.direction == 'up'){
+    return 'right'
+  } else if (mouse.top == 175 && mouse.direction == 'down') {
+    return 'left'
+  } else if (mouse.left == 25 && mouse.direction == 'left') {
+    return 'up'
+  } else if (mouse.left == 325 && mouse.direction == 'right') {
+    return 'down'
+  } else {
+    return mouse.direction
+  }
+}
 
 if (Meteor.isClient) {
   Template.ccr.mice = function() {
@@ -18,19 +44,9 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.setInterval(function() {
     Mice.find({}).forEach(function (mouse) {
-      function move(direction){
-        switch(direction) {
-          case "up":
-            return {$inc: {top: -50}}
-          case "down":
-            return {$inc: {top: 50}}
-          case "left":
-            return {$inc: {left: -50}}
-          case "right":
-            return {$inc: {left: 50}}
-        }
-      }
-      Mice.update(mouse._id, move(mouse.direction));
+      var new_direction = directionAfterWall(mouse);
+      Mice.update(mouse._id, {$set: {direction: new_direction }});
+      Mice.update(mouse._id, move(new_direction));
     });
   }, 1000)
 
