28b58c632eb8739be1bb701522cd5942d3424525
SoonKhen OwYong <dude@owyong.sk>
2014-06-14 15:53:57 +0800

    added scoring and removing mice

--- chu_chu_rocket.js
+++ chu_chu_rocket.js
@@ -1,4 +1,5 @@
 Mice = new Meteor.Collection("mice");
+Players = new Meteor.Collection("players");
 
 SpawnPoints = [[25,25,'right'],[75,325,'left']] // only two
 
@@ -17,16 +18,31 @@ function move(direction){
 
 function directionAfterWall(mouse){
   if (mouse.top == 25 && mouse.direction == 'up'){
-    return 'right'
+    return 'right';
   } else if (mouse.top == 175 && mouse.direction == 'down') {
-    return 'left'
+    return 'left';
   } else if (mouse.left == 25 && mouse.direction == 'left') {
-    return 'up'
+    return 'up';
   } else if (mouse.left == 325 && mouse.direction == 'right') {
-    return 'down'
+    return 'down';
   } else {
     return mouse.direction
   }
+}
+
+function scorePoint(mouse){
+  if (mouse.top == 75 && mouse.left == 25){
+    Players.update(Players.findOne({number: 1})._id, {$inc: {score: 1}});
+  } else if (mouse.top == 175 && mouse.left == 25) {
+    Players.update(Players.findOne({number: 1})._id, {$inc: {score: 1}});
+  } else if (mouse.top == 25 && mouse.left == 325) {
+    Players.update(Players.findOne({number: 2})._id, {$inc: {score: 1}});
+  } else if (mouse.top == 125 && mouse.left == 325) {
+    Players.update(Players.findOne({number: 2})._id, {$inc: {score: 1}});
+  } else {
+    return;
+  }
+  Mice.remove(mouse._id);
 }
 
 if (Meteor.isClient) {
@@ -37,6 +53,9 @@ if (Meteor.isClient) {
       mice[i].left -= 25;
     });
     return mice;
+  }
+  Template.ccr_score.players = function() {
+    return Players.find({});
   }
 
   // Template.ccr.events({
@@ -52,13 +71,15 @@ if (Meteor.isServer) {
       var new_direction = directionAfterWall(mouse);
       Mice.update(mouse._id, {$set: {direction: new_direction }});
       Mice.update(mouse._id, move(new_direction));
+      scorePoint(mouse);
     });
   }, 50)
 
   Meteor.startup(function () {
     // code to run on server at startup
 
-    Mice.remove({}); // remove all mice
+    Mice.remove({});    // remove all mice
+    Players.remove({}); // remove all players
 
     _.each(SpawnPoints, function(sp) {
       Mice.insert({
@@ -68,5 +89,13 @@ if (Meteor.isServer) {
       });
     });
 
+    Players.insert({
+      number: 1,
+      score: 0
+    });
+    Players.insert({
+      number: 2,
+      score: 0
+    });
   });
 }
