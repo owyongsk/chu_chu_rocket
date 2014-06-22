3d38633abae2bb6e896fbba85b8ce257da0bf348
Edward Fung <edwardfung123@gmail.com>
2014-06-22 11:29:34 +0800

    WIP put all DB update/remove in the end of the game loop
    Signed-off-by: Edward Fung <edwardfung123@gmail.com>

--- chu_chu_rocket.js
+++ chu_chu_rocket.js
@@ -34,12 +34,77 @@ var UP = 'up';
 var LEFT = 'left';
 var DOWN = 'down';
 
-var sps = [
-    _.extend({player: 1, direction: UP}   , grid2pixel(6,3))
-  , _.extend({player: 1, direction: RIGHT}, grid2pixel(8,3))
-  , _.extend({player: 2, direction: DOWN} , grid2pixel(8,5))
-  , _.extend({player: 2, direction: LEFT} , grid2pixel(6,5))
+var sps_easy = [
+    // single one
+    _.extend({direction: UP}   , grid2pixel(MAX_COL-2,2))
 ];
+
+var sps_real_hard = [
+    // Very difficult one
+    _.extend({direction: UP}   , grid2pixel(1,MAX_ROW-1))
+  , _.extend({direction: UP}   , grid2pixel(2,MAX_ROW-1))
+  , _.extend({direction: UP}   , grid2pixel(3,MAX_ROW-1))
+  , _.extend({direction: UP}   , grid2pixel(4,MAX_ROW-1))
+  , _.extend({direction: UP}   , grid2pixel(5,MAX_ROW-1))
+  , _.extend({direction: UP}   , grid2pixel(6,MAX_ROW-1))
+  , _.extend({direction: UP}   , grid2pixel(7,MAX_ROW-1))
+  , _.extend({direction: UP}   , grid2pixel(8,MAX_ROW-1))
+  , _.extend({direction: UP}   , grid2pixel(9,MAX_ROW-1))
+  , _.extend({direction: UP}   , grid2pixel(10,MAX_ROW-1))
+  , _.extend({direction: UP}   , grid2pixel(11,MAX_ROW-1))
+  , _.extend({direction: UP}   , grid2pixel(12,MAX_ROW-1))
+  , _.extend({direction: UP}   , grid2pixel(13,MAX_ROW-1))
+
+  , _.extend({direction: RIGHT}   , grid2pixel(0,1))
+  , _.extend({direction: RIGHT}   , grid2pixel(0,2))
+  , _.extend({direction: RIGHT}   , grid2pixel(0,3))
+  , _.extend({direction: RIGHT}   , grid2pixel(0,4))
+  , _.extend({direction: RIGHT}   , grid2pixel(0,5))
+  , _.extend({direction: RIGHT}   , grid2pixel(0,6))
+  , _.extend({direction: RIGHT}   , grid2pixel(0,7))
+
+  , _.extend({direction: LEFT}   , grid2pixel(MAX_COL-1,1))
+  , _.extend({direction: LEFT}   , grid2pixel(MAX_COL-1,2))
+  , _.extend({direction: LEFT}   , grid2pixel(MAX_COL-1,3))
+  , _.extend({direction: LEFT}   , grid2pixel(MAX_COL-1,4))
+  , _.extend({direction: LEFT}   , grid2pixel(MAX_COL-1,5))
+  , _.extend({direction: LEFT}   , grid2pixel(MAX_COL-1,6))
+  , _.extend({direction: LEFT}   , grid2pixel(MAX_COL-1,7))
+
+  , _.extend({direction: DOWN}   , grid2pixel(1,0))
+  , _.extend({direction: DOWN}   , grid2pixel(2,0))
+  , _.extend({direction: DOWN}   , grid2pixel(3,0))
+  , _.extend({direction: DOWN}   , grid2pixel(4,0))
+  , _.extend({direction: DOWN}   , grid2pixel(5,0))
+  , _.extend({direction: DOWN}   , grid2pixel(6,0))
+  , _.extend({direction: DOWN}   , grid2pixel(7,0))
+  , _.extend({direction: DOWN}   , grid2pixel(8,0))
+  , _.extend({direction: DOWN}   , grid2pixel(9,0))
+  , _.extend({direction: DOWN}   , grid2pixel(10,0))
+  , _.extend({direction: DOWN}   , grid2pixel(11,0))
+  , _.extend({direction: DOWN}   , grid2pixel(12,0))
+  , _.extend({direction: DOWN}   , grid2pixel(13,0))
+
+];
+
+var sps_normal = [
+    _.extend({direction: UP}   , grid2pixel(6,3))
+  , _.extend({direction: UP}   , grid2pixel(5,2))
+  , _.extend({direction: UP}   , grid2pixel(4,1))
+
+  , _.extend({direction: RIGHT}, grid2pixel(8,3))
+  , _.extend({direction: RIGHT}, grid2pixel(9,2))
+  , _.extend({direction: RIGHT}, grid2pixel(10,1))
+
+  , _.extend({direction: DOWN} , grid2pixel(8,5))
+  , _.extend({direction: DOWN} , grid2pixel(9,6))
+  , _.extend({direction: DOWN} , grid2pixel(10,7))
+
+  , _.extend({direction: LEFT} , grid2pixel(4,7))
+  , _.extend({direction: LEFT} , grid2pixel(5,6))
+  , _.extend({direction: LEFT} , grid2pixel(6,5))
+];
+var sps = sps_normal;
 
 var destinations = [
     _.extend(grid2pixel(0,0), {pid: 1})
@@ -48,6 +113,7 @@ var destinations = [
   , _.extend(grid2pixel(MAX_COL-1,MAX_ROW-1), {pid: 4})
 ];
 
+// return the change for move & direction
 function move(direction){
   switch(direction) {
     case "up":
@@ -105,6 +171,27 @@ function spawnCat(){
     case 3: dir = 'right'; break;
   }
   Cats.insert({left:x, top:y, direction: dir});
+}
+
+// any A collides with any B, the cb will be called.
+function collideWith(objAs, objBs, cb){
+  cb = cb || function(){};
+  _.each(objAs, function(a,i){
+    _.each(objBs, function(b,j){
+      if (a.top == b.top && a.left == b.left){
+        cb(a, b);
+      }
+    });
+  });
+}
+
+// Return which destination the obj is stepping on.
+function getOnDestination(obj){
+  var dst = _.find(destinations, function(dst){
+    return mouse.top == dst.top && mouse.left == dst.left;
+  });
+
+  return dst;
 }
 
 function scorePoint(mouse, op, cls){
@@ -287,56 +374,129 @@ if (Meteor.isServer) {
       lock = true;
       var toUpdate = [];
 
-      Mice.find({}).forEach(function (mouse) {
-        mouse.direction = directionAfterArrow(mouse);
-        var new_direction =  directionAfterWall(mouse);
-        data = _.extend({}, {$set: {direction: new_direction }}, move(new_direction));
-        Mice.update(mouse._id, data);
-        scorePoint(mouse, {$inc: {score: MICE_SCORE}}, Mice);
-      });
+      var mice = Mice.find({}).fetch();
+      var playersChanges = {
+        1: {}
+        , 2: {}
+        , 3: {}
+        , 4: {}
+      };
 
-      // Before cats move, mouse collides cat.
-      var cats =  Cats.find().fetch();
-      _.each(cats, function(cat){
-        var pos = {left: cat.left, top: cat.top};
-        Mice.find(pos).forEach(function(m){
-          Mice.remove(m);
-        });
-      });
+      collideWith(mice, destinations, function(m, d){
+        // Remove the mouse (just mark it to be removed)
+        m.remove = true;
 
-      // Cat move
-      _.each(cats, function(cat){
-        cat.direction = directionAfterArrow(cat, function(arrow){
-          var catp = _.pick(cat, 'top', 'left');
-          console.log('to be removed dbArrows = ', arrow);
-          Arrows.remove(arrow);
-          var index = -1;
-          var arrowId = _.find(arrows, function(a, i){
-            console.log('a = ' + a +  ' aid = ' + arrow._id);
-            if ( a == arrow._id ){
-              index = i;
-              return true;
-            }
-            return false;
-          });
-          if (arrowId){
-            arrows['' + arrow.pid].splice(index, 1);
+        // Update the player change
+        if (!playersChanges[d.pid].$inc){
+          playersChanges[d.pid].$inc = {score: 1};
+        } else {
+          if (!playersChanges[d.pid].$inc.score){
+            playersChanges[d.pid].$inc.score = 1;
+          } else {
+            playersChanges[d.pid].$inc.score++;
           }
-        });
-        var new_direction = directionAfterWall(cat);
-        data = _.extend({}, {$set: {direction: new_direction }}, move(new_direction));
-        Cats.update(cat._id, data);
-        scorePoint(cat, {$inc: {score: -CAT_PUNISH_SCORE}}, Cats);
+        }
       });
 
-      // cat moved, cat collide with mouse
-      cats =  Cats.find().fetch();
-      _.each(cats, function(cat){
-        var pos = {left: cat.left, top: cat.top};
-        Mice.find(pos).forEach(function(m){
-          Mice.remove(m);
-        });
+      _.each(mice, function(mouse, i){
+        if (!mouse.remove){  // undefined || null || false
+          mouse.direction = directionAfterArrow(mouse);
+          var new_dir = directionAfterWall(mouse);
+          mice[i].change = _.extend({}
+              , mice[i].change
+              , {$set: {direction: new_dir}}
+              , move(new_dir)
+          );
+        }
       });
+
+      mice = _.groupBy(mice, function(m){
+        return m.remove ? 'remove' : 'stay';
+      });
+      console.log('update mices:', mice);
+
+      // remove the mices
+      if (mice.remove){
+        var toRemove = _.pluck(mice.remove, '_id');
+        console.log('toRemove = ', toRemove);
+        Mice.remove({_id: {$in: toRemove}});
+      }
+
+      // other mice that should stay in the game will move
+      if (mice.stay){
+        _.each(mice.stay, function(m, i){
+          Mice.update(m._id, m.change);
+        });
+      }
+
+
+      // Update the player's score
+      console.log('update players:', playersChanges);
+
+      _.each(playersChanges, function(p, n){
+        if (_.size(p)){
+          var change = p;
+          var id = players[n];
+          console.log('going to update players[' + n + '](' + id + ')' + JSON.stringify(change));
+          Players.update(id, change);
+        }
+      });
+
+
+      // Move the mice.
+      // Arrow collision was detected before move so that we can find new direction of the coming move.
+      // .detection after mouse move.
+      //Mice.find({}).forEach(function (mouse) {
+      //_.each(mice, function(mouse, i){
+      //  mouse.direction = directionAfterArrow(mouse);
+      //  var new_direction =  directionAfterWall(mouse);
+      //  data = _.extend({}, {$set: {direction: new_direction }}, move(new_direction));
+      //  Mice.update(mouse._id, data);
+      //  scorePoint(mouse, {$inc: {score: MICE_SCORE}}, Mice);
+      //});
+
+      //// Before cats move, mouse collides cat.
+      //var cats =  Cats.find().fetch();
+      //_.each(cats, function(cat){
+      //  var pos = {left: cat.left, top: cat.top};
+      //  Mice.find(pos).forEach(function(m){
+      //    Mice.remove(m);
+      //  });
+      //});
+
+      //// Cat move
+      //_.each(cats, function(cat){
+      //  cat.direction = directionAfterArrow(cat, function(arrow){
+      //    var catp = _.pick(cat, 'top', 'left');
+      //    console.log('to be removed dbArrows = ', arrow);
+      //    Arrows.remove(arrow);
+      //    var index = -1;
+      //    var arrowId = _.find(arrows, function(a, i){
+      //      console.log('a = ' + a +  ' aid = ' + arrow._id);
+      //      if ( a == arrow._id ){
+      //        index = i;
+      //        return true;
+      //      }
+      //      return false;
+      //    });
+      //    if (arrowId){
+      //      arrows['' + arrow.pid].splice(index, 1);
+      //    }
+      //  });
+      //  var new_direction = directionAfterWall(cat);
+      //  data = _.extend({}, {$set: {direction: new_direction }}, move(new_direction));
+      //  Cats.update(cat._id, data);
+      //  scorePoint(cat, {$inc: {score: -CAT_PUNISH_SCORE}}, Cats);
+      //});
+
+      //// cat moved, cat collide with mouse
+      //cats =  Cats.find().fetch();
+      //_.each(cats, function(cat){
+      //  var pos = {left: cat.left, top: cat.top};
+      //  Mice.find(pos).forEach(function(m){
+      //    Mice.remove(m);
+      //  });
+      //});
 
 
       lock = false;
@@ -355,6 +515,7 @@ if (Meteor.isServer) {
       delete sp._id;
       Mice.insert(sp);
     });
+
     //t++;
     //console.log(t);
   }, spawningRate);
